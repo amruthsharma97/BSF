0a9a52cc79513e1f9d10efe9553d1a807447c076
amruthsharma97 <amruthsharma97@gmail.com>
2019-02-07 18:21:49 +0530

    project created

---
+++ BSF Project/BSF Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BSF_Project.MvcApplication" Language="C#" %>
---
+++ BSF Project/BSF Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BSF_Project.Startup))]
+namespace BSF_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
